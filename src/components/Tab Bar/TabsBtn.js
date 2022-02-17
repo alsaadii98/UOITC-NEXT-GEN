b670b319aaf8a6678bca15b14117b819766dbde6
Ali H.Alsaadi <a.h.alsaady1998@gmail.com>
2022-02-17 18:20:35 +0300

    93%

--- src/components/Tab Bar/TabsBtn.js
+++ src/components/Tab Bar/TabsBtn.js
@@ -6,32 +6,37 @@ import WorkShops from "./WorkShops";
 
 const TabsBtn = (props) => {
   const [toggle, setToggle] = useState(1);
-  const activeClass =
-    "animate-pulse  bg-red-100 shadow-lg shadow-gray-200 text-red-800   h-full transition-all duration-500 ease-in-out rounded-2xl duration-500 w-36 import active:outline-none focus:outline-none ";
+
   return (
-    <div className="flex mt-20 flex-col items-center justify-center space-y-10">
+    <div className="flex mt-10 flex-col items-center justify-center space-y-10">
       {/* Tabs */}
-      <div className="sticky  top-0 flex items-center justify-between justify-items-center shadow-lg shadow-gray-200  bg-white h-16 lg:w-[25rem] rounded-2xl">
+      <div className="sticky border-2 border-gray-100 top-0 flex items-center justify-between justify-items-center shadow-lg shadow-gray-200  bg-white h-20  lg:w-[24rem] rounded-2xl">
         <button
           onClick={() => setToggle(1)}
-          className={`w-32 focus:outline-none ${
-            toggle === 1 ? activeClass : "w-28"
+          className={`focus:outline-none ${
+            toggle === 1
+              ? "text-sm sm:text-md border-r-2 border-gray-100 shadow-lg shadow-gray-200 text-red-800 h-full transition-all duration-500 ease-in-out rounded-l-2xl  w-28  sm:w-32 import active:outline-none focus:outline-none "
+              : "text-sm sm:text-md w-28 sm:w-32"
           } `}
         >
           Conference
         </button>
         <button
           onClick={() => setToggle(2)}
-          className={`w-32 focus:outline-none ${
-            toggle === 2 ? activeClass : "w-28"
+          className={`focus:outline-none ${
+            toggle === 2
+              ? "text-sm sm:text-md border-r-2 border-l-2 border-gray-100 shadow-lg shadow-gray-200 text-red-800 h-full transition-all ease-in-out  duration-500  w-28 sm:w-32 import active:outline-none focus:outline-none "
+              : "text-sm sm:text-md w-28 sm:w-28"
           } `}
         >
           Seminars
         </button>
         <button
           onClick={() => setToggle(3)}
-          className={`w-32 focus:outline-none ${
-            toggle === 3 ? activeClass : "w-28"
+          className={`focus:outline-none ${
+            toggle === 3
+              ? "text-sm sm:text-md border-l-2 border-gray-100 shadow-lg shadow-gray-200 text-red-800 h-full transition-all  ease-in-out rounded-r-2xl duration-500  w-28 sm:w-32 import active:outline-none focus:outline-none "
+              : "text-sm sm:text-md w-28 sm:w-32"
           } `}
         >
           Workshops
