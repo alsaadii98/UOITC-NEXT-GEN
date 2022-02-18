6d0574b49f5d5961a5fc0735138bb9d7cd410d19
Alsaadii98 <a.h.alsaady1998@gmail.com>
2022-02-18 23:21:52 +0300

    THE PROJECT IS DONE

--- src/Pages/Article.js
+++ src/Pages/Article.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 import { useParams } from "react-router-dom";
- function Article() {
-  const {id} = useParams();
-  
+import ArticleCom from "../components/Article page/ArticleCom";
+import useAxios from "../Hooks/useAxios";
+
+function Article() {
+  const { id } = useParams();
+  const { dataIn, loading, error } = useAxios(
+    "http://localhost:8000/articles/" + id
+  );
   return (
     <div>
       <Navbar />
-      <div className='text-5xl text-center mt-44'>{id}</div>
       <div>
-        
+        {loading && <div>Loading ...</div>}
+        {error && <div>{error}</div>}
+        {dataIn && <ArticleCom id={id} text={dataIn.text} title={dataIn.title} date={dataIn.date} />}
       </div>
+
       <Footer />
     </div>
-  )
+  );
 }
-export default Article
+export default Article;
--- src/Pages/Articles.js
+++ src/Pages/Articles.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-
+import  ArticlesCom  from "../components/Articles page/ArticlesCom";
 function Articles() {
   
   return (
     <div>
       <Navbar />
-      
-      {/* <Footer /> */}
+      <ArticlesCom/>
+      <Footer />
     </div>
   );
 }
